888bf3458581ac371cb223f30463bffff4297f5d
Bruz Marzolf <bruz@bruzilla.com>
2013-10-15 06:32:35 -0700

    Pulling and saving statistic sets

---
+++ src/hotframeworks/models/statistic_set.clj
@@ -0,0 +1,85 @@
+(ns hotframeworks.models.statistic-set
+  (:require [hotframeworks.models.db :as db]
+            [hotframeworks.statistic-types.awis :as awis]
+            [hotframeworks.statistic-types.github :as github]
+            [hotframeworks.statistic-types.stackoverflow :as stackoverflow]))
+
+(def statistic-types [:github :stackoverflow :traffic :links])
+
+(defn stat [framework type]
+  (do
+    (prn (str "Pulling " (name type) " stat for " (framework :name) "..."))
+    (cond
+      (= type :github ) (github/stat framework)
+      (= type :stackoverflow ) (stackoverflow/stat framework)
+      (= type :traffic ) (awis/traffic-stat framework)
+      (= type :links ) (awis/link-stat framework))))
+
+(defn stats [frameworks type]
+  (map (fn [framework]
+         {:framework_id (:id framework)
+          :value (stat framework type)}) frameworks))
+
+(defn log-values [stats]
+  (map (fn [stat]
+         (let [value (:value stat)]
+           (if (nil? value)
+             nil
+             (Math/log value)))) stats))
+
+(defn score-value [value min-value max-value]
+  (let [offset (- (Math/log value) min-value)
+        range (- max-value min-value)]
+    (int (* (/ offset range) 100))))
+
+(defn score-stats [stats]
+  (let [stat-values (log-values stats)
+        non-nil (remove nil? stat-values)
+        min-value (apply min non-nil)
+        max-value (apply max non-nil)]
+    (map (fn [stat]
+           (if-let [value (:value stat)]
+             (assoc stat :score (score-value value min-value max-value))
+             nil))
+      stats)))
+
+(defn last-set []
+  (let [set (db/last-statistic-set)]
+    (if set
+      (group-by :type (db/statistics-for-set set))
+      nil)))
+
+(defn delta-set [type new-stats]
+  (if-let [last-stats (get (last-set) (name type))]
+    {type (map (fn [new-stat]
+                 (let [id (:framework_id new-stat)
+                       last-stat (first (filter #(= id (:framework_id %)) last-stats))
+                       new-score (:score new-stat)
+                       last-score (:score last-stat)]
+                   (assoc new-stat :delta (- new-score last-score)))) new-stats)}
+    {type new-stats}))
+
+(defn pull []
+  (let [frameworks (db/all-frameworks)]
+    (into {}
+      (map (fn [type]
+             (delta-set type
+               (score-stats
+                 (stats frameworks type))))
+        statistic-types))))
+
+(defn save! [data]
+  (let [set (db/add-statistic-set! (:date data))
+        id (set :id)]
+    (doseq [[type stats] (:stats data)]
+      (doseq [stat stats]
+        (when (:value stat)
+          (db/add-statistic!
+            (assoc stat :statistic_set_id id :type (name type))))))
+    id))
+
+(defn today []
+  (new java.sql.Date (.getTime (new java.util.Date))))
+
+(defn pull-and-save! []
+  (save! {:date (today) :stats (pull)}))
---
+++ test/hotframeworks/test/models/statistic_set.clj
@@ -0,0 +1,131 @@
+(ns hotframeworks.test.models.statistic-set
+  (:require [hotframeworks.models.statistic-set :as statistic-set]
+            [hotframeworks.models.db :as db]
+            [hotframeworks.statistic-types.github :as github]
+            [hotframeworks.statistic-types.stackoverflow :as stackoverflow]
+            [hotframeworks.statistic-types.awis :as awis])
+  (:use midje.sweet))
+
+(def tentacles {:id 1 :github_owner "Raynes" :github_repo "tentacles"})
+(def noir {:id 2 :github_owner "noir-clojure" :github_repo "noir"})
+(def rails {:id 3 :github_owner "rails" :github_repo "rails"})
+
+(against-background [(db/all-frameworks) => [tentacles noir rails]
+                     (github/stat tentacles) => 12
+                     (github/stat noir) => 321
+                     (github/stat rails) => 1200
+                     (stackoverflow/stat tentacles) => 90
+                     (stackoverflow/stat noir) => 50
+                     (stackoverflow/stat rails) => 400
+                     (awis/traffic-stat tentacles) => 2
+                     (awis/traffic-stat noir) => 4
+                     (awis/traffic-stat rails) => 20
+                     (awis/link-stat tentacles) => 40
+                     (awis/link-stat noir) => 300
+                     (awis/link-stat rails) => 200000]
+
+  (fact "about pulling stats with a prior stat set"
+    (statistic-set/pull) => {:github
+                             [{:framework_id 1 :value 12 :score 0 :delta 0}
+                              {:framework_id 2 :value 321 :score 71 :delta 11}
+                              {:framework_id 3 :value 1200 :score 100 :delta 0}]
+                             :stackoverflow
+                             [{:framework_id 1 :value 90 :score 28 :delta 28}
+                              {:framework_id 2 :value 50 :score 0 :delta -71}
+                              {:framework_id 3 :value 400 :score 100 :delta 0}]
+                             :traffic
+                             [{:delta 0 :score 0 :framework_id 1 :value 2}
+                              {:delta -41 :score 30 :framework_id 2 :value 4}
+                              {:delta 0 :score 100 :framework_id 3 :value 20}]
+                             :links
+                             [{:delta 0 :score 0 :framework_id 1 :value 40}
+                              {:delta -48 :score 23 :framework_id 2 :value 300}
+                              {:delta 0 :score 100 :framework_id 3 :value 200000}]}
+
+    (provided (db/last-statistic-set) => {:id 42 :date "DATE"})
+
+        (provided (db/statistics-for-set {:id 42 :date "DATE"}) => [{:framework_id 1 :value 10 :score 0 :type "github"}
+                                                                    {:framework_id 2 :value 280 :score 60 :type "github"}
+                                                                    {:framework_id 3 :value 1200 :score 100 :type "github"}
+                                                                    {:framework_id 1 :value 100 :score 0 :type "stackoverflow"}
+                                                                    {:framework_id 2 :value 40 :score 71 :type "stackoverflow"}
+                                                                    {:framework_id 3 :value 420 :score 100 :type "stackoverflow"}
+                                                                    {:framework_id 1 :value 2 :score 0 :type "traffic"}
+                                                                    {:framework_id 2 :value 3 :score 71 :type "traffic"}
+                                                                    {:framework_id 3 :value 21 :score 100 :type "traffic"}
+                                                                    {:framework_id 1 :value 35 :score 0 :type "links"}
+                                                                    {:framework_id 2 :value 390 :score 71 :type "links"}
+                                                                    {:framework_id 3 :value 19900 :score 100 :type "links"}]))
+
+(fact "about pulling stats without a prior stat set"
+  (statistic-set/pull) => {:github
+                           [{:framework_id 1 :value 12 :score 0}
+                            {:framework_id 2 :value 321 :score 71}
+                            {:framework_id 3 :value 1200 :score 100}]
+                           :stackoverflow
+                           [{:framework_id 1 :value 90 :score 28}
+                            {:framework_id 2 :value 50 :score 0}
+                            {:framework_id 3 :value 400 :score 100}]
+                           :traffic
+                           [{:framework_id 1 :value 2 :score 0}
+                            {:framework_id 2 :value 4 :score 30}
+                            {:framework_id 3 :value 20 :score 100}]
+                           :links
+                           [{:framework_id 1 :value 40 :score 0}
+                            {:framework_id 2 :value 300 :score 23}
+                            {:framework_id 3 :value 200000 :score 100}]}
+
+  (provided (db/last-statistic-set) => [])))
+
+(def today (statistic-set/today))
+
+(def pulled-stats {:date today
+                   :stats {:github [{:framework_id 1 :value 12 :score 0 :delta 0}
+                                   {:framework_id 2 :value 321 :score 71 :delta 11}
+                                   {:framework_id 3 :value 1200 :score 100 :delta 0}
+                                   {:framework_id nil :value nil :score nil :delta nil}]
+                          :stackoverflow [{:framework_id 1 :value 90 :score 28 :delta 28}
+                                          {:framework_id 2 :value 50 :score 0 :delta -71}
+                                          {:framework_id 3 :value 400 :score 100 :delta 0}
+                                          {:framework_id nil :value nil :score nil :delta nil}]}})
+
+(fact "about saving stats"
+  (statistic-set/save! pulled-stats) => 1
+  (provided (db/add-statistic-set! today) => {:id 1})
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 1
+                               :type "github"
+                               :value 12
+                               :score 0
+                               :delta 0}) => 1)
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 2
+                               :type "github"
+                               :value 321
+                               :score 71
+                               :delta 11}) => 2)
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 3
+                               :type "github"
+                               :value 1200
+                               :score 100
+                               :delta 0}) => 3)
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 1
+                               :type "stackoverflow"
+                               :value 90
+                               :score 28
+                               :delta 28}) => 4)
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 2
+                               :type "stackoverflow"
+                               :value 50
+                               :score 0
+                               :delta -71}) => 5)
+  (provided (db/add-statistic! {:statistic_set_id 1
+                               :framework_id 3
+                               :type "stackoverflow"
+                               :value 400
+                               :score 100
+                               :delta 0}) => 6)
+)
